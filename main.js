5a2cf888d31ea90794348d316b575254435d56f0
sridharhitman <83956232+sridharhitman@users.noreply.github.com>
2021-09-22 14:16:22 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+var name_of_the_student_array = [];
+
+function submit() {
+    var display_student_array = [];
+
+    for (var i = 1; i <= 4; i++) {
+        var name_of_the_student = document.getElementById("name_of_the_student_" + i).value;
+        name_of_the_student_array.push(name_of_the_student);
+    }
+    var length_of_name_of_students = name_of_the_student_array.length;
+    for (var h = 0; h < length_of_name_of_students; h++) {
+        display_student_array.push("<h4> NAME - " + name_of_the_student_array[k] + "</h4>");
+        console.log(display_student_array);
+    }
+    document.getElementById("display_name_with_commas").innerHTML = display_student_array;
+    var remove_commas = display_student_array.join(" ");
+    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
+    document.getElementById("submit_button").style.display = "none";
+    document.getElementById("sort_button").style.display = "inline-block";
+}
+
+function sorting() {
+    name_of_the_student_array.sort();
+    console.log(name_of_the_student_array);
+    var display_student_array_sorting = [];
+    var lenght_of_name_of_students_array = name_of_the_student_array.length;
+    console.log(lenght_of_name_of_students_array);
+    for (var k = 0; k < lenght_of_name_of_students_array; k++) {
+        display_student_array_sorting.push("NAME -" + name_of_the_student_array[k] + " < /h4>"); 
+        console.log(display_student_array_sorting); } var remove_commas = display_student_array_sorting.join(" "); 
+        console.log(remove_commas); document.getElementById("display_name_without_commas").innerHTML = remove_commas; }
